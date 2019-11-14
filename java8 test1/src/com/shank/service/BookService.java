df97e02523f8e452ace7cc75ebdff59de1132f55
764752 <764752@PC331428.cts.com>
2019-11-14 18:36:50 +0530

    erfgsdf

--- java8 test1/src/com/shank/service/BookService.java
+++ java8 test1/src/com/shank/service/BookService.java
@@ -1,6 +1,5 @@
 package com.shank.service;
 
-
 import java.util.Collections;
 
 import java.util.List;
@@ -9,21 +8,16 @@ import com.shank.book.Book;
 import com.shank.bookDao.BookDao;
 
 public class BookService {
-	
-public List<Book> getBooksInSort()
-{
-	List<Book> books=new BookDao().getBooks();
-Collections.sort(books,(o1,o2) ->  o1.getName().compareTo(o2.getName()));
-	
-	return books;
-	
-}
 
-public static void main(String args[])
-{
-	System.out.println(new BookService().getBooksInSort());
-}
-}
+	public List<Book> getBooksInSort() {
+		List<Book> books = new BookDao().getBooks();
+		Collections.sort(books, (o1, o2) -> o1.getName().compareTo(o2.getName()));
 
+		return books;
 
-	
+	}
+
+	public static void main(String args[]) {
+		System.out.println(new BookService().getBooksInSort());
+	}
+}
